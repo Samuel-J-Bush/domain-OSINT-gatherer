4b48992a7c2aab998216693c20b995e3e7dd1e15
Samuel-J-Bush <samueljackbush@gmail.com>
2026-07-23 06:19:07 +0100

    Add certificate collection functionality
    Implement certificate collection for a given domain, extracting issuer and validity dates.

---
+++ collectors/certificate_collector.py
@@ -0,0 +1,29 @@
+import ssl
+import socket # Necessary imports for ssl and socket connections
+
+def cert_collection(domain):
+    cert_results = {
+        "domain": domain,
+        "issuer": "",
+        "valid_from":"",
+        "valid_until":"",
+    }
+
+    try:
+        context = ssl.create_default_context() # Create the secure connection context
+        connection = socket.create_connection((domain, 443), timeout = 15) # Connect to domain on port 443 (HTTPS)
+        wrapped_connection = context.wrap_socket(connection, server_hostname = domain) # Wrap the connection with SSL
+
+        certificate = wrapped_connection.getpeercert()
+        wrapped_connection.close() # Connection can now be closed as we have certificate so now pull data from that
+
+        issuer_list = certificate["issuer"] # Extract the issuer list from cert
+        for issuer in issuer_list:
+            issuer_name = issuer[0][1] # Extracts the name of the organisation that issued the certificate
+            cert_results["issuer"] = issuer_name # Feed back data to dictionary
+
+        cert_results['valid_from'] = certificate['notBefore']  # Store valid from data in results, in the certificate it is called notBefore
+        cert_results['valid_until'] = certificate['notAfter']  # Store valid until data in results, in the certificate it is called notAfter
+    
+    except: # Catch any erros so doesnt stop the whole collection if a certificate collection error
+        pass
