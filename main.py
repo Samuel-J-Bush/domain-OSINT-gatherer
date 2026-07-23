69945ff95be4b4e197ff0ac2afe581e427d324ab
Samuel-J-Bush <samueljackbush@gmail.com>
2026-07-23 06:22:55 +0100

    Add domain information collection main script
    Implement the main script that calls upon all the collectors in the collectors folder.

---
+++ main.py
@@ -0,0 +1,30 @@
+from collectors.dns_collector import dns_collection # So the functions that collect the data in their section are callable
+from collectors.http_collector import http_collection
+from collectors.certificate_collector import cert_collection
+from collectors.file_collector import file_collection 
+from report import report_generator # So the report_generator function can imporve formatting of the dictionaries to be outputted
+
+domain = input("Enter domain name: ") # User can select the domain to gather info about
+option = input("Do you want output in terminal or output in a file. Enter t or f: ").lower() # So user can choose between terminal output or file creation
+
+dns_results = dns_collection(domain) # Contains the data in the results variables so they can be put as arguments for report_generator
+print("DNS collection complete")
+http_results = http_collection(domain)
+print("http collection complete")
+cert_results = cert_collection(domain)
+print("Certificate collection complete")
+file_results = file_collection(domain)
+print("File collection complete")
+
+total_report = report_generator(dns_results, http_results, cert_results, file_results, domain) # In order to have a better formatted, easier to read output.
+
+if option == "f":
+    filename = f"{domain}_information.txt" # So the filename is recongisable as info on the domain
+    with open(filename, "w", encoding = "utf-8") as newfile: # Opening file in "write" makes a new file or overwrites if report for domain already exists
+        newfile.write(total_report)
+
+elif option == "t":
+    print(total_report) # So user can see results in terminal
+
+else: 
+    print("Input error, please run again and enter t or f")
