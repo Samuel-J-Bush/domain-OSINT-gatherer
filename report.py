26beb5a414d95123dc637ccf3990adfdf5010506
Samuel-J-Bush <samueljackbush@gmail.com>
2026-07-29 15:30:12 +0100

    Enhance report formatting with section headers

--- report.py
+++ report.py
@@ -7,6 +7,18 @@ def report_generator(dns_results, http_results, cert_results, file_results, whoi
     report_text = report_text +  f"Gathered information for {domain} on {formatted_time}\n" # To clearly state the domain once at the top alongside date and time
 
     for results_type in [dns_results, http_results, cert_results, file_results, whois_results]: # For each of the types of results, iterates through the four result dictionaries.
+        
+        if results_type == dns_results: # The selection to put in correct headers for each section
+            report_text = report_text + f"\nDNS RESULTS \n"
+        elif results_type == http_results:
+            report_text = report_text + f"\nHTTP RESULTS \n"
+        elif results_type == cert_results:
+            report_text = report_text + f"\nCERTIFICATE RESULTS \n"
+        elif results_type == file_results:
+            report_text = report_text + f"\nFILE RESULTS \n"
+        elif results_type == whois_results:
+            report_text = report_text + f"\nWHOIS RESULTS \n"
+        
         for rtype, record in results_type.items(): # Loops through each key value pair in the dictionary
 
             if rtype == "domain": # Domain is in all dictionaries, this ensures the domain isnt repeated as it is already put in by line 3
